c4bc5f0636ae5d2ac4928e719ccc67f705120f33
Austen Cameron <austen.cameron@gmail.com>
2020-12-02 18:30:56 -0700

    wip

--- src/ComponentConcerns/RendersLivewireComponents.php
+++ src/ComponentConcerns/RendersLivewireComponents.php
@@ -2,6 +2,14 @@
 
 namespace Livewire\ComponentConcerns;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\PhpEngine;
+use Livewire\Exceptions\BypassViewHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
+
 trait RendersLivewireComponents
 {
     protected $livewireComponent;
@@ -48,5 +56,36 @@ trait RendersLivewireComponents
         }
 
         return ltrim(ob_get_clean());
+    }
+
+    // Errors thrown while a view is rendering are caught by the Blade
+    // compiler and wrapped in an "ErrorException". This makes Livewire errors
+    // harder to read, AND causes issues like `abort(404)` not actually working.
+    protected function handleViewException(Throwable $e, $obLevel)
+    {
+        if ($this->shouldBypassExceptionForLivewire($e, $obLevel)) {
+            // This is because there is no "parent::parent::".
+            PhpEngine::handleViewException($e, $obLevel);
+
+            return;
+        }
+
+        CompilerEngine::handleViewException($e, $obLevel);
+    }
+
+    public function shouldBypassExceptionForLivewire(Throwable $e, $obLevel)
+    {
+        $uses = array_flip(class_uses_recursive($e));
+
+        return (
+            // Don't wrap "abort(403)".
+            $e instanceof AuthorizationException
+            // Don't wrap "abort(404)".
+            || $e instanceof NotFoundHttpException
+            // Don't wrap "abort(500)".
+            || $e instanceof HttpException
+            // Don't wrap most Livewire exceptions.
+            || isset($uses[BypassViewHandler::class])
+        );
     }
 }
--- src/LivewireViewCompilerEngine.php
+++ src/LivewireViewCompilerEngine.php
@@ -4,6 +4,7 @@ namespace Livewire;
 
 use Exception;
 use Throwable;
+use Illuminate\View\Engines\CompilerEngine;
 use Livewire\ComponentConcerns\RendersLivewireComponents;
 
 class LivewireViewCompilerEngine extends CompilerEngine
